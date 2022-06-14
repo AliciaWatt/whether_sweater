d7853e07ca3dfa06b5a9c46e5db65698d753af1b
Alicia Watt <alicia.watt.aw@gmail.com>
2022-06-14 09:37:47 -0600

    refactor: forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def search
-    latLon = MapQuestFacade.get_geocode("#{params[:city]}, #{params[:state]}")
-    lat = latLon[:lat]
-    lon = latLon[:lng]
-    forecast = OpenWeatherFacade.get_geocode(lat, lon)
+    latLon = MapQuestFacade.get_geocode(params[:location])
+
+    forecast = OpenWeatherFacade.get_forecast(latLon[:lat], latLon[:lng])
     json_response(ForecastSerializer.forecast(forecast), :ok)
   end
 end
