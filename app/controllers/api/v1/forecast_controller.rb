1c1ebcc351b7c73cc60da1d37db682cebc4737b1
Alicia Watt <alicia.watt.aw@gmail.com>
2022-07-02 09:31:12 -0700

    setup

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
   def search
-    # latLon = MapQuestFacade.get_geocode(params[:location])
-    #
-    # forecast = OpenWeatherFacade.get_forecast(latLon[:lat], latLon[:lng])
     forecast = ForecastFacade.get_forecast(params[:location].to_s)
     json_response(ForecastSerializer.forecast(forecast), :ok)
   end
